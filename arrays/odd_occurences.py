e2144210b7685fa472907455acac621fd249a501
Cristian Hainic <cristihainic@gmail.com>
2019-10-30 00:30:34 +0200

    corrected odd_occurences

--- arrays/odd_occurences.py
+++ arrays/odd_occurences.py
@@ -1,11 +1,12 @@
 def solution(A):
-    nums = ''.join(str(i) for i in A)
-    for num in nums:
-        if nums.count(num) % 2 != 0:
+    for num in A:
+        if A.count(num) % 2 != 0:
             return int(num)
 
 # test cases
 x = [9, 3, 9, 3, 9, 7, 9]
 y = [9, 9, 3, 3, 9, 7, 7]
+z = [42]
 assert solution(x) == 7
 assert solution(y) == 9
+assert solution(z) == 42
